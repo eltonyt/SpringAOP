b17302e2df8f788becf7a1228fa5dd7a85cdb821
Elton Li <elton.li@ubenefit.net>
2022-10-27 00:09:09 -0500

    joinPoint - read method signature and parameter information

--- src/main/java/com/elton/SpringAOP/aspect/ComboAspect.java
+++ src/main/java/com/elton/SpringAOP/aspect/ComboAspect.java
@@ -1,8 +1,11 @@
 package com.elton.SpringAOP.aspect;
 
+import com.elton.SpringAOP.Account;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -11,7 +14,26 @@ import org.springframework.stereotype.Component;
 @Order(1)
 public class ComboAspect {
     @Before("com.elton.SpringAOP.aspect.LuvAopExpressionUtils.forDaoNoSettersOrGetters()")
-    public void beforeDoingAnything(){
+    public void beforeDoingAnything(JoinPoint theJoinPoint){
         System.out.println("\n=================> This is happening before all methods except getters and setters.");
+
+//        DISPLAY THE METHOD SIGNATURE
+        MethodSignature methodSig = (MethodSignature) theJoinPoint.getSignature();
+        System.out.println("Method: " + methodSig) ;
+
+//        DISPLAY METHOD ARGUMENTS
+//        GET ARGS
+        Object[] args = theJoinPoint.getArgs();
+
+//        LOOP THROUGH ARGS
+        for (Object tempArg : args){
+            System.out.println(tempArg);
+            if (tempArg instanceof Account){
+                Account theAccount = (Account) tempArg;
+                System.out.println("Account Name: " + theAccount.getName());
+                System.out.println("Account Level: " + theAccount.getLevel());
+            }
+        }
+
     }
 }
