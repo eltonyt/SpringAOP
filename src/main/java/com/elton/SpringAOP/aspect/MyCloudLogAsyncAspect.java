c6b0e6e0205f3c273e91c27ee3359cb10b6213f3
Elton Li <elton.li@ubenefit.net>
2022-10-28 23:44:54 -0500

    Use Logger instead of SOUT to fix the order issue

--- src/main/java/com/elton/SpringAOP/aspect/MyCloudLogAsyncAspect.java
+++ src/main/java/com/elton/SpringAOP/aspect/MyCloudLogAsyncAspect.java
@@ -9,17 +9,20 @@ import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 @Component
 @Aspect
 @Order(3)
 public class MyCloudLogAsyncAspect {
 
+    private Logger myLogger = Logger.getLogger(getClass().getName());
+    
     @Around("execution(* com.elton.SpringAOP.service.*.getFortune(..))")
     public Object aroundGetFortune(ProceedingJoinPoint theProceedingJoinPoint) throws Throwable{
         // print out method we are advising on
         String method = theProceedingJoinPoint.getSignature().toShortString();
-        System.out.println("\n===========>>>>>> Executing @Around on method: " + method);
+        myLogger.info("\n===========>>>>>> Executing @Around on method: " + method);
 
         //get begin timestamp
         long begin = System.currentTimeMillis();
@@ -32,14 +35,14 @@ public class MyCloudLogAsyncAspect {
 
         //calculate the time window
         long duration = end - begin;
-        System.out.println("\n ======> Duration: " + duration / 1000.0 + " seconds");
+        myLogger.info("\n ======> Duration: " + duration / 1000.0 + " seconds");
         return result;
     }
 
     @After("execution(* com.elton.SpringAOP.dao.AccountDAO.findAccounts(..))")
     public void afterFinallyFindAccountAdvice(JoinPoint theJoinPoint) {
         String method = theJoinPoint.getSignature().toShortString();
-        System.out.println("\n===========>>>>>> Executing @After (finally) on method: " + method);
+        myLogger.info("\n===========>>>>>> Executing @After (finally) on method: " + method);
     }
 
     @AfterThrowing(
@@ -48,9 +51,9 @@ public class MyCloudLogAsyncAspect {
     public void afterThrowingFindAccountsAdvice(JoinPoint theJoinPoint, Throwable theExc) {
 
         String method = theJoinPoint.getSignature().toShortString();
-        System.out.println("\n===========>>>>>> Excuting @AfterReturning on method: " + method);
+        myLogger.info("\n===========>>>>>> Excuting @AfterReturning on method: " + method);
 
-        System.out.println("\n===========>>>>>> The exception is: " + theExc);
+        myLogger.info("\n===========>>>>>> The exception is: " + theExc);
     }
 
     // ADD A NEW ADVICE FOR @AFTERRETURNING ON THE FINDACCOUNTS METHOD
@@ -61,10 +64,10 @@ public class MyCloudLogAsyncAspect {
 
         // PRINT OUT WHICH METHOD WE ARE ADVISING ON
         String method = theJoinPoint.getSignature().toShortString();
-        System.out.println("\n===========>>>>>> Excuting @AfterReturning on method: " + method);
+        myLogger.info("\n===========>>>>>> Excuting @AfterReturning on method: " + method);
 
         // PRINT OUT THE RESULTS OF THE METHOD CALL
-        System.out.println("\n===========>>>>>> result is: " + result);
+        myLogger.info("\n===========>>>>>> result is: " + result);
 
         // LET'S POST-PROCESS THE DATA ... MODIFY IT :-)
 
@@ -83,6 +86,6 @@ public class MyCloudLogAsyncAspect {
 
     @Before("com.elton.SpringAOP.aspect.LuvAopExpressionUtils.forDaoNoSettersOrGetters()")
     public void logToCloud(){
-        System.out.println("\n=================> Storing Log.");
+        myLogger.info("\n=================> Storing Log.");
     }
 }
